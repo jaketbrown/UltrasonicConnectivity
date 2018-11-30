43e503b02e271d45305a6b63bee541c089813990
jantony1 <jantony789@gmail.com>
2018-11-29 19:58:34 -0500

    FULLY INTEGRATED

--- HoneyGuide/app/src/main/java/com/project/cmsc436/honeyguide/MainActivity.java
+++ HoneyGuide/app/src/main/java/com/project/cmsc436/honeyguide/MainActivity.java
@@ -105,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         };
-
+        chirpConnect.setListener(connectEventListener);
         chirpConnect.start();
 
     }
